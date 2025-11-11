abae55cb257becd3be0211d50e35a245ceade151
Ellensramalho <ellen_santos_bsf@hotmail.com>
2025-11-10 21:37:05 -0300

    Novos scripts de testes.

--- cypress/e2e/testesFormulario2.cy.js
+++ cypress/e2e/testesFormulario2.cy.js
@@ -3,5 +3,14 @@ describe('Visitar formulário', () => {
     cy.visit('https://formulario-para-testes-com-cypress.vercel.app/')
   })
 
+  it('Verificar título da página', () => {
+  cy.contains('h1', 'Formulário para testes').should('be.visible')
 })
+//O campo de senha deve aceitar no máximo 15 caracteres
+  it.only('Validar número máximo de caracteres na senha', () => {
+    cy.get('#senha').type('testandotamanhomaximo')
+    cy.get('#senha').should('have.attr', 'maxlength', '15')
 
+  })
+
+})
